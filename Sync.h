3d5cc2644e4b0a66d21ebb29ec4dfa4122771d65
unknown <lyn.deneuve@gmail.com>
2013-11-23 21:12:04 +0100

    Add Sync class definition

--- Sync.h
+++ Sync.h
@@ -1,4 +1,33 @@
 #ifndef SYNC_H
 #define SYNC_H
 
-#endif // SYNC_H
+#include <QWidget>
+
+class QGridLayout;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+class QTextEdit;
+class QVBoxLayout;
+
+class Sync : public QWidget {
+    Q_OBJECT
+
+public:
+    Sync();
+
+private:
+    QLabel *location1Label;
+    QLineEdit *location1PathLineEdit;
+    QPushButton *location1BrowseButton;
+    QLabel *location2Label;
+    QLineEdit *location2PathLineEdit;
+    QPushButton *location2BrowseButton;
+    QTextEdit *statusTextEdit;
+    QPushButton *synchronizeButton;
+
+    QVBoxLayout *mainLayout;
+    QGridLayout *locationsLayout;
+};
+
+#endif
